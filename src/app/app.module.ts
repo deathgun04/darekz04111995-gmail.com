64afb89110ea433d4654c720b72e671bc286b061
deathgun04 <darekz04111995@gmail.com>
2020-04-16 10:33:07 +0300

    final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import {BrowserModule} from '@angular/platform-browser'
-import {NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import {AppComponent} from './app.component'
-import {FormsModule} from '@angular/forms'
-import {StyleDirective} from './directives/style.directive';
-
+import {AppComponent} from './app.component';
+import {FormsModule} from '@angular/forms';
+import {PostFormComponent} from './post-form/post-form.component';
+import {PostComponent} from './post/post.component';
 @NgModule({
   declarations: [
     AppComponent,
-    StyleDirective
+    PostFormComponent,
+    PostComponent,
   ],
   imports: [
     BrowserModule,
